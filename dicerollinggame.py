29ca72ee98d800436fca3b185ae31125fd231c3a
Calvin javid <kalvinjaved32@gmail.com>
2025-09-03 11:51:15 -0700

    Update dicerollinggame.py

--- dicerollinggame.py
+++ dicerollinggame.py
@@ -1,10 +1,15 @@
 import random
-num1 = random.randint(1,6)
-num2 = random.randint(1,6)
-a =  input("press y if you want to roll and n if you don't")
 
-if a == "y" or a == "Y":
- print(f"({num1},{num2})")
-else:
 
- print("no problem")
+while True:
+ a =  input("press y if you want to roll and n if you don't =")
+
+     
+     
+ if a.lower() == "y":
+   num1 = random.randint(1,6)
+   num2 = random.randint(1,6)
+   print(f"({num1},{num2})")
+ else:
+  print("no problem")
+  break
